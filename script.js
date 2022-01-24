0bc247e5ce25bb891298927760779369c4a05ef8
Mindshaked <eduardgb92@gmail.com>
2022-01-24 20:57:05 +0100

    Second draft

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
 
-let arr = ["Rock", "Paper", "Scissors"];
+const arr = ["rock", "paper", "scissors"];
+
+
+
+
 
 function computerPlay(){
-    let computerSelection = Math.random() * 
+    let numberSelection = Math.random() * 3;
+    let computerChoice = arr [Math.floor(numberSelection)];
+    return computerChoice;
 
 }
 
-function playerSelection(humanChoice){
+function playerSelection(){
     let playerInput = prompt("What do you choose, rock, paper or scissors?");
-    let playerSelection = playerInput.toLowerCase();
+    let playerChoice = playerInput.toLowerCase();
+    return playerChoice;
 
 }
+
+
+
+let playerWins = 0;
+let computerWins = 0;
 
 
 function playRound(playerSelection, computerSelection){
+    
+    
     if (playerSelection == computerSelection){
         console.log("tie");
-    } else if (){
+    
 
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        computerWins++;
+        console.log("Computer Wins!");
+        return computerWins;
+
+    } else if (playerSelection == "rock" && computerSelection == "scissors"){
+        playerWins++;
+        console.log("You win!");
+        return playerWins;
+        
+
+    } else if (playerSelection == "paper" && computerSelection == "rock"){
+        playerWins++;
+        console.log("You win!");
+        return playerWins;
+        
+    } else if (playerSelection == "paper" && computerSelection == "scissors"){
+        computerWins++;
+        console.log("Computer wins!");
+        return computerWins;
+        
     }
+        
 
 
 }
 
 
 
-console.log(playRound(playerSelection, computerSelection));
+const playerSelection = "Rock";
+const computerSelection = computerPlay();
+
+console.log(playRound(playerSelection, computerSelection))
