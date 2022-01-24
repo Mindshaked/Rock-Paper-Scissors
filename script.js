844f862b2a4a3bbab54bd35c396d8f3244ffc04e
Mindshaked <eduardgb92@gmail.com>
2022-01-24 20:23:47 +0100

    first draft

---
+++ script.js
@@ -0,0 +1,28 @@
+
+let arr = ["Rock", "Paper", "Scissors"];
+
+function computerPlay(){
+    let computerSelection = Math.random() * 
+
+}
+
+function playerSelection(humanChoice){
+    let playerInput = prompt("What do you choose, rock, paper or scissors?");
+    let playerSelection = playerInput.toLowerCase();
+
+}
+
+
+function playRound(playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        console.log("tie");
+    } else if (){
+
+    }
+
+
+}
+
+
+
+console.log(playRound(playerSelection, computerSelection));
