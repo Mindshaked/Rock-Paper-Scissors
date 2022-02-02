35e6ddfa7ca1b4af41c6b570c071d13a2c73d4bf
Mindshaked <eduardgb92@gmail.com>
2022-02-02 19:00:33 +0100

    finished functionality, style pending

--- script.js
+++ script.js
@@ -33,6 +33,11 @@ function playerSelectionScissors(){
 let playerWins = 0;
 let computerWins = 0;
 
+// Result markers
+let playerResultMarker = document.getElementById("playerResult")
+
+let computerResultMarker = document.getElementById("computerResult");
+console.log(computerResultMarker);
 
 function playRound(playerChoice, computerChoice){
     
@@ -42,38 +47,46 @@ function playRound(playerChoice, computerChoice){
 
     } else if (playerChoice == "rock" && computerChoice == "paper"){
         computerWins++;
+        computerResultMarker.textContent++;
         console.log("Computer Wins!");
         return computerWins;
 
     } else if (playerChoice == "rock" && computerChoice == "scissors"){
         playerWins++;
+        playerResultMarker.textContent++;
         console.log("You win!");
         return playerWins;
         
 
     } else if (playerChoice == "paper" && computerChoice == "rock"){
         playerWins++;
+        playerResultMarker.textContent++;
         console.log("You win!");
         return playerWins;
         
     } else if (playerChoice == "paper" && computerChoice == "scissors"){
         computerWins++;
+        computerResultMarker.textContent++;
         console.log("Computer wins!");
         return computerWins;
         
     } else if (playerChoice == "scissors" && computerChoice == "paper"){
         playerWins++;
+        playerResultMarker.textContent++;
         console.log("You win!");
         return playerWins;
                 
     } else if (playerChoice == "scissors" && computerChoice == "rock"){
         computerWins++;
+        computerResultMarker.textContent++;
         console.log("Computer wins!");
         return computerWins;
     }
 
 
 }
+
+
 
 /*function playFullGame (){
     for (let rounds = 0; rounds < 6; rounds++){
@@ -87,6 +100,8 @@ function playRound(playerChoice, computerChoice){
 
 */
 
+//linkear los resultados a las variables.
+
 let buttons = document.querySelectorAll('.buttonChoices');
 
 buttons.forEach((button) => {
@@ -95,40 +110,54 @@ buttons.forEach((button) => {
             let playerChoice = "rock";
             let computerChoice = computerPlay();
             let winner = playRound(playerChoice,computerChoice);
-            console.log(winner);
+            console.log(winner, computerWins, playerWins);
+            return winner;
         } else if (e.target.id == "paperButton"){
             let playerChoice = "paper";
             let computerChoice = computerPlay();
             let winner = playRound(playerChoice,computerChoice);
-            console.log(winner);
+            console.log(winner, computerWins, playerWins);
+            return winner;
         } else if (e.target.id == "scissorsButton"){
             let playerChoice = "scissors";
             let computerChoice = computerPlay();
             let winner = playRound(playerChoice,computerChoice);
-            console.log(winner);
-        }
+            console.log(winner, computerWins, playerWins);
+            return winner;
+        } 
 
     });
 });
 
 
-
-/*playerSelection.addEventListener("click", function() => {
-    if (buttonChoices.class == rockButton) {
-        let playerChoice = "rock"; 
-        computerPlay(); 
-        playRound();
-}   else if (button.id == paperButton){
-        let playerChoice = "paper";
-        computerPlay();
-        playRound();
-}   else if (button.id == scissorsButton){
-        let playerChoice = "scissors";
-        computerPlay();
-        playRound();
+//button to rest the game
+function resetGame(){
+    playerWins = 0;
+    computerWins = 0;
+    
 }
+let resetButton = document.querySelector('.reset');
+
+resetButton.addEventListener('click', function(e) {
+    resetGame();;
+    computerResultMarker.textContent = "0";
+    playerResultMarker.textContent = "0";
+
+    console.log(playerWins,computerWins);
 });
+
+
+
+if (playerWins == 5){
+    console.log("Player wins the game");
+} else if (computerWins == 5){
+    console.log("Computer wins the game");
+}
+
+
+
+
+//console.log(computerWins + playerWins)
 
 //playFullGame ()
 
-*/
